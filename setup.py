6641b9306f5d0bd80bf0fa0cf3175bfcefdef951
David Mote <damote@ucsd.edu>
2011-09-22 13:36:13 -0700

    initial setup

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+setup(name='ari.policy',
+      version=version,
+      description="The collection of products required for the Ari website",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'':'src'},
+      namespace_packages=['ari'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+          'avrc.ari.theme',
+          'collective.uploadify',
+          'collective.indexing',
+          'jyu.z3cform.datepicker',    
+          'beast.cache',
+          'plone.app.ldap',
+          'Products.PloneFormGen',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
---
+++ src/ari/policy/testing.py
@@ -0,0 +1,36 @@
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+
+from plone.testing import z2
+
+from zope.configuration import xmlconfig
+
+class ariPolicy(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+    
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML
+        import ari.policy
+        xmlconfig.file('configure.zcml', ari.policy, context=configurationContext)
+        
+        # Install products that use an old-style initialize() function
+        z2.installProduct(app, 'Products.PythonField')
+        z2.installProduct(app, 'Products.TALESField')
+        z2.installProduct(app, 'Products.TemplateFields')
+        z2.installProduct(app, 'Products.PloneFormGen')
+    
+    def tearDownZope(self, app):
+        # Uninstall products installed above
+        z2.uninstallProduct(app, 'Products.PloneFormGen')
+        z2.uninstallProduct(app, 'Products.TemplateFields')
+        z2.uninstallProduct(app, 'Products.TALESField')
+        z2.uninstallProduct(app, 'Products.PythonField')
+        
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'ari.policy:default')
+
+ARI_POLICY_FIXTURE = leadthewayPolicy()
+ARIY_POLICY_INTEGRATION_TESTING = IntegrationTesting(bases=(ARI_POLICY_FIXTURE,), name="ari:Integration")
